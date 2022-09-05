0e1a8ef69bd3daf3c283b24988b9d3d2e528e5f8
Salanto <62221668+Salanto@users.noreply.github.com>
2022-09-05 23:16:50 +0200

    Version bump to v0.2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication App(argc, argv);
-    App.setApplicationVersion("0.1");
+    App.setApplicationVersion("0.2.0");
     App.setApplicationName("AO2 Char.ini Editor v1");
     AO2CharMaker ui_window;
     ui_window.show();
